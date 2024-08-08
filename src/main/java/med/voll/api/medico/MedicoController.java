1ac4d204097998ea500092fafb7d898d2e553c33
Rodrigo Najdek <rodrigo.najdek@gmail.com>
2024-08-08 14:54:20 -0300

    Aula 05 parte 01

--- src/main/java/med/voll/api/medico/MedicoController.java
+++ src/main/java/med/voll/api/medico/MedicoController.java
@@ -1,4 +1,4 @@
-package med.voll.api.controller;
+package med.voll.api.medico;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -6,16 +6,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import med.voll.api.medico.DadosCadastroMedico;
-import med.voll.api.medico.DadosListagemMedico;
-import med.voll.api.medico.Medico;
-import med.voll.api.medico.MedicoRepository;
 
 @RestController
 @RequestMapping("medicos")
@@ -33,6 +30,12 @@ public class MedicoController {
 	@GetMapping
 	public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
 		return repository.findAll(paginacao).map(DadosListagemMedico::new);
+	}
+	
+	@PutMapping
+	@Transactional
+	public void atualizar(@RequestBody @Valid DadosCadastroMedico dados) {
+		
 	}
 	
 }
